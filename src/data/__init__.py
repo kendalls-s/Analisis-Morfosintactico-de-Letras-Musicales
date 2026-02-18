458fe02b8849329beff84d313a29610c65c43b4a
Betocoto <117998100+Betocoto@users.noreply.github.com>
2026-02-18 17:21:02 -0600

    Update __init__.py

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,12 @@
-#Llamar variables
-from .loader import carga_original,carga_limpios
+
+from .loader import carga_original, carga_limpios
 from .cleaner import limpiar_dataset
-from .preprocessor import token_nltk
-__all__ = ["carga_limpios","carga_original", "limpiar_dataset","token_nltk"]
+from .preprocessor import token_nltk, token_spacy
+
+__all__ = [
+    "carga_limpios",
+    "carga_original",
+    "limpiar_dataset",
+    "token_nltk",
+    "token_spacy"
+]
