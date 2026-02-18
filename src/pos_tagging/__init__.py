17509a00d8a43b4026ca0d8a5beeecaa5f043a84
Betocoto <117998100+Betocoto@users.noreply.github.com>
2026-02-18 17:21:22 -0600

    Update __init__.py

--- src/pos_tagging/__init__.py
+++ src/pos_tagging/__init__.py
@@ -1,3 +1,7 @@
 #Llamar variables
+from .Taggin_spacy import apply_pos_tagging_spacy
 from .nltk_tagger import apply_pos_tagging
-__all__ = ["apply_pos_tagging"]
+__all__ = ["apply_pos_tagging", "apply_pos_tagging_spacy"]
+
+
+
