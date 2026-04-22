bfd1e8569595681a9189f496c574d8cf09a043c2
Betocoto <117998100+Betocoto@users.noreply.github.com>
2026-04-22 00:57:01 -0600

    Update __init__.py

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,9 +1,14 @@
+from werkzeug.wsgi import responder
+
 from .loader import carga_original, carga_limpios, carga_pos_nltk,carga_pos_spacy
 from .cleaner import limpiar_dataset_csv
 from .preprocessor import token_nltk, token_spacy
 from .mongo_storage import guardar_pos_tags,guardar_embeddings,guardar_metricas,insertar_canciones
 from .lyrics_cleaner import limpiar_letra
-__all__ = [
+from ..Proyecto_3.finetuning_utils import preparar_dataset,entrenar_clasificador, evaluar_modelo, cargar_clasificador, predecir_genero
+from ..Proyecto_3.rag_utils import _get_model, chunk_por_cancion,chunk_por_estrofa, construir_indice,buscar_chunks
+from ..Proyecto_3.chatbot_engine import _get_generator,construir_prompt, responder, limpiar_historial, get_historial
+__all__ = {
     "carga_limpios",
     "carga_original",
     "carga_pos_nltk",
@@ -15,5 +20,22 @@ __all__ = [
     "guardar_embeddings",
     "limpiar_letra",
     "insertar_canciones",
-    "guardar_metricas"
-]
+    "guardar_metricas",
+    "_get_model",
+    "chunk_por_cancion",
+    "chunk_por_estrofa",
+    "construir_indice",
+    "buscar_chunks",
+    "preparar_dataset",
+    "entrenar_clasificador",
+    "evaluar_modelo",
+    "cargar_clasificador",
+    "predecir_genero",
+    "_get_generator",
+    "construir_prompt",
+    "responder",
+    'limpiar_historial',
+    "get_historial"
+
+}
+
